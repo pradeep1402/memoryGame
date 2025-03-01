66c0e02f6b645115a3251d1451ac2d9171255454
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2025-03-01 23:00:12 +0530

    cards are closing at same time

--- script/index.js
+++ script/index.js
@@ -1,13 +1,43 @@
+const clearAllTimeouts = (setTimeoutsId) => {
+  for (const timeoutId of setTimeoutsId) {
+    clearInterval(timeoutId);
+  }
+
+  setTimeoutsId.clear();
+};
+
+const closeAllOpenedCards = (clickedCards) => {
+  for (const clickedCard of clickedCards) {
+    clickedCard.classList.remove('open_card');
+  }
+
+  clickedCards.clear();
+};
+
+const openCards = (clickedCards) => {
+  for (const clickedCard of clickedCards) {
+    clickedCard.classList.add('open_card');
+  }
+};
+
 const startGame = () => {
-  const card = document.querySelectorAll('.card');
+  const setTimeoutsId = new Set();
+  const clickedCards = new Set();
 
   addEventListener("click", (event) => {
-    event.target.style.backgroundColor = 'black';
+    if (!(event.target.classList.contains('card'))) return;
 
-    setTimeout(() => {
-      event.target.style.backgroundColor = 'lightgreen';
+    clickedCards.add(event.target);
+    openCards(clickedCards);
+    clearAllTimeouts(setTimeoutsId);
+
+    const timeoutId = setTimeout(() => {
+      closeAllOpenedCards(clickedCards);
     }, 2000);
+
+    setTimeoutsId.add(timeoutId);
   });
+
 };
 
 window.onload = startGame;
