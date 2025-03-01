79bc0762bb01433771abd74fcedce38b5c3b14df
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2025-03-02 00:03:26 +0530

    cards are removed if color is same

--- script/index.js
+++ script/index.js
@@ -7,16 +7,29 @@ const clearAllTimeouts = (setTimeoutsId) => {
 };
 
 const closeAllOpenedCards = (clickedCards) => {
-  for (const clickedCard of clickedCards) {
-    clickedCard.classList.remove('open_card');
-  }
-
-  clickedCards.clear();
+  return setTimeout(() => {
+    if (areColorMatches(clickedCards)) removeCards(clickedCards);
+    for (const clickedCard of clickedCards) {
+      clickedCard.classList.remove('open_card');
+    }
+    clickedCards.clear();
+  }, 2000);
 };
 
 const openCards = (clickedCards) => {
   for (const clickedCard of clickedCards) {
     clickedCard.classList.add('open_card');
+  }
+};
+
+const areColorMatches = ([...clickedCards]) => {
+  return clickedCards[0].className === clickedCards[1]?.className;
+
+};
+
+const removeCards = (clickedCards) => {
+  for (const clickedCard of clickedCards) {
+    clickedCard.style.display = 'none';
   }
 };
 
@@ -31,10 +44,7 @@ const startGame = () => {
     openCards(clickedCards);
     clearAllTimeouts(setTimeoutsId);
 
-    const timeoutId = setTimeout(() => {
-      closeAllOpenedCards(clickedCards);
-    }, 2000);
-
+    const timeoutId = closeAllOpenedCards(clickedCards);
     setTimeoutsId.add(timeoutId);
   });
 
